2553cc5a682cb41ed4fb9a8189b829e1d9e42dae
tmarunagiri <tmarunagiri.tiruchendurminnurathanam@pearson.com>
2019-04-01 18:06:25 +0530

    forgotpassword page designed

--- src/app/forgotpassword/forgotpassword.page.ts
+++ src/app/forgotpassword/forgotpassword.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgotpassword',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgotpassword.page.scss'],
 })
 export class ForgotpasswordPage implements OnInit {
-
-  constructor() { }
+  isValidOTP: boolean = false;
+  pinNo: number;
+  repinNo:number;
+  isPinValid: boolean = true;
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+  checkOTP() {
+    this.isValidOTP = true;
+  }
+  changePassword() {
 
+  }
+  cancel() {
+    this.router.navigateByUrl("/login");
+  }
+  isNumeric() {
+    this.isPinValid=true;
+    let keyb = <KeyboardEvent>event;
+    let keycode = keyb.which ? keyb.which : keyb.keyCode;
+    return keycode >= 48 && keycode <= 57;
+  }
+  changePin(){
+    if(this.pinNo!=this.repinNo){
+      this.isPinValid=false;
+    }
+  }
+  cancelOTP(){
+    this.router.navigateByUrl("/login");
+  }
 }
