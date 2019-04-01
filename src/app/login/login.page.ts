e8830924b5428f08875ea5723967efdadfc2e0c4
tmarunagiri <tmarunagiri.tiruchendurminnurathanam@pearson.com>
2019-04-01 18:05:35 +0530

    login page designed

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {ToastController} from "@ionic/angular";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  beforeLogin: boolean = true;
+  isValid: boolean = false;
+  mobileNo: string="";
+  pinNo: number;
+  constructor(private router: Router,private toastController: ToastController) { }
 
   ngOnInit() {
+  }
+  login() {
+    this.beforeLogin = false;
+  }
+  forgotPassword = () => {
+    if(this.mobileNo.length <10){
+      this.presentToast("Please enter a valid mobile No.");
+      return;
+    }
+    this.router.navigateByUrl("/forgotpassword");
+  }
+  signUp() {
+    this.router.navigateByUrl("/signup");
+  }
+  cancelVerify() {
+    this.beforeLogin = true;
+  }
+  isNumeric() {
+    let keyb = <KeyboardEvent>event;
+    let keycode = keyb.which ? keyb.which : keyb.keyCode;
+    return keycode >= 48 && keycode <= 57;
+  }
+  async presentToast(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
